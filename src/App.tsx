df86b345639ae11c8fdd27ebfe704ca7ae56d6e2
Natalia M Zaballos <natmarzab@proton.me>
2025-07-16 10:53:56 +0200

    [conluzweb-2] Added CardTemplate, SupplyCard, CardList and SupplyPointsPage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import './App.css'
 import { Route, Routes } from 'react-router'
 import { AuthenticatedLayout } from './layouts/authenticated.layout'
-import ContactPage from './pages/contact/Contact.page'
+import { LoginLayout } from './layouts/login.layout'
+import { Login } from './components/login/login'
+import { ForgotPassword } from './components/ForgotPassword/ForgotPassword'
+import { NewPassword } from './components/NewPassword/NewPassword'
+import { SupplyPointsPage } from './components/supplyPoints/SupplyPointsPage'
 
 function App() {
 
   return (
     <>
       <Routes>
+        <Route element={<LoginLayout/>}>
+          <Route path='login' element={<Login/>}></Route>
+          <Route path='forgot-password'>
+            <Route index element={<ForgotPassword/>}></Route>
+            <Route path=':token' element={<NewPassword/>}></Route>
+          </Route>
+        </Route> 
         <Route element={<AuthenticatedLayout/>}>
-          <Route index />
-          <Route path="contacto" element={<ContactPage />}/> 
+          {/* <Route index element={SupplyPointsPage}/> */}
+          <Route path='supply-points' element={<SupplyPointsPage/>}/>
+
+          
         </Route>
       </Routes>
     </>
---
+++ src/utils/types.tsx
@@ -0,0 +1,10 @@
+export type SupplyPointData = {
+    supplyPointId: number,
+    kWh: number,
+    lastCheckTime: string,
+    supplyPointName: string,
+    address: string,
+    average: number,
+    status: string,
+
+}
